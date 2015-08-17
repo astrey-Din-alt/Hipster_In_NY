ad30a13ad245d99349a4c4dff440f5f3ba8cb91f
astrey-Din-alt <a.cherkashin@gmail.com>
2015-08-17 15:33:54 +0300

    Content update. Add Enemy, make him move. After hit make him dead.

---
+++ Assets/Scripts/HurtEnemy.cs
@@ -0,0 +1,16 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class HurtEnemy : MonoBehaviour {
+
+	private GameObject enemy;
+
+	void OnTriggerEnter2D(Collider2D col)
+	{
+		if(col.gameObject.tag == "Player")
+		{
+			enemy = GameObject.FindGameObjectWithTag("Enemy");
+			enemy.GetComponent<Enemy>().Hurt();
+		}
+	}
+}

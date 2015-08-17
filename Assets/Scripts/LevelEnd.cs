88bd5e17d86a6bd963bd003e61574f0bcfdfc000
astrey-Din-alt <a.cherkashin@gmail.com>
2015-08-17 15:11:00 +0300

    Content update. Add level End icon and create method.

---
+++ Assets/Scripts/LevelEnd.cs
@@ -0,0 +1,24 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class LevelEnd : MonoBehaviour {
+	void OnTriggerEnter2D(Collider2D col)
+	{
+		// If the player hits the trigger...
+		if (col.gameObject.tag == "Player") {
+				// ... destroy the player.
+				Destroy (col.gameObject);
+				// ... reload the level.
+				StartCoroutine ("LoadNextLevel");
+				Debug.LogError ("LoadNextLevel!");
+	
+		}
+	}
+	IEnumerator LoadNextLevel()
+	{	
+		float fadeTime = GameObject.Find("_GM").GetComponent<Fading>().BeginFade(1);
+		yield return new WaitForSeconds(fadeTime);
+		Application.LoadLevel (Application.loadedLevel + 1);
+	}		
+}
+
